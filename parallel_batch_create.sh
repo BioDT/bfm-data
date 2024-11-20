602a6621eaf95343d516e9b03e104fb6a5f9c268
Martino Mensio <martino.mensio@tno.nl>
2024-11-20 16:39:04 +0100

    time limit 30 minutes, it should take 15 minutes

--- parallel_batch_create.sh
+++ parallel_batch_create.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=batch_create
 # rome and genoa cause OOM, let's go GPU that has more RAM
 #SBATCH --partition=gpu_a100
-#SBATCH --time=6:00:00
+#SBATCH --time=0:30:00
 # SBATCH --nodes=1
 # SBATCH --ntasks-per-node=1
 # SBATCH --cpus-per-task=18

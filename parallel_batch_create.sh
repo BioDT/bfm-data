2be8c4a4b23c86682ed463ddaeb9e81292f1a174
Martino Mensio <martino.mensio@tno.nl>
2024-11-22 08:56:32 +0000

    cleanup script

--- parallel_batch_create.sh
+++ parallel_batch_create.sh
@@ -2,16 +2,14 @@
 #SBATCH --job-name=batch_create
 # rome and genoa cause OOM, let's go GPU that has more RAM
 #SBATCH --partition=gpu_a100
+# takes around 22 minutes per batch, but let's put 1 hour to be safe
 #SBATCH --time=01:00:00
-# SBATCH --nodes=1
-# SBATCH --ntasks-per-node=1
-# SBATCH --cpus-per-task=18
 #SBATCH --gpus-per-node=1
 
 
 # use:
 # sbatch -a 0-735 parallel_batch_create.sh
-# first run the following:
+# first run the following to generate the ERA5_days_pairs.json file:
 # python src/dataset_creation/parallel_batch.py create-list-file
 
 echo "SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID"

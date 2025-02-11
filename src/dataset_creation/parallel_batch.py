4e956a2dd676cdc6a056fbd5a69893525569719c
Martino Mensio <martino.mensio@tno.nl>
2025-02-11 16:22:48 +0000

    remove duplicate path declarations

--- src/dataset_creation/parallel_batch.py
+++ src/dataset_creation/parallel_batch.py
@@ -6,7 +6,14 @@ import json
 
 import typer
 
-from src.config.paths import DATA_DIR, STORAGE_DIR
+from src.config.paths import (
+    AGRICULTURE_COMBINED_FILE,
+    ERA5_DIR,
+    FOREST_FILE,
+    LAND_COMBINED_FILE,
+    SPECIES_DATASET,
+    SPECIES_EXTINCTION_FILE,
+)
 from src.dataset_creation.create_dataset import (
     create_batches_for_pair_of_days,
     get_paths_for_files_pairs_of_days,
@@ -23,18 +30,18 @@ app = typer.Typer(pretty_exceptions_enable=False)
 # the file to save the potential pairs of days for batch
 list_file_path = "ERA5_days_pairs.json"
 
-era5_directory = f"{DATA_DIR}/Copernicus/ERA5"
+era5_directory = ERA5_DIR
 
-species_file = f"{STORAGE_DIR}/processed_data/species_dataset.parquet"
-agriculture_file = f"{DATA_DIR}/Agriculture/Europe_combined_agriculture_data.csv"
-land_file = f"{DATA_DIR}/Land/Europe_combined_land_data.csv"
-forest_file = f"{DATA_DIR}/Forest/Europe_forest_data.csv"
-species_extinction_file = f"{DATA_DIR}/RedList/Europe_red_list_index.csv"
+species_file = SPECIES_DATASET
+agriculture_file = AGRICULTURE_COMBINED_FILE
+land_file = LAND_COMBINED_FILE
+forest_file = FOREST_FILE
+species_extinction_file = SPECIES_EXTINCTION_FILE
 
 
 @app.command()
 def create_list_file():
-    all_pairs = get_paths_for_files_pairs_of_days(era5_directory)
+    all_pairs = get_paths_for_files_pairs_of_days(str(era5_directory))
     json_list = []
     for day_1_paths, day_2_paths in all_pairs:
         json_list.append(
@@ -69,11 +76,11 @@ def run_single(index: int):
     single_dataset_day2_path = selected["day_2"]["single"]
     surface_dataset_day2_path = selected["day_2"]["surface"]
 
-    species_dataset = load_species_data(species_file)
-    agriculture_dataset = load_world_bank_data(agriculture_file)
-    land_dataset = load_world_bank_data(land_file)
-    forest_dataset = load_world_bank_data(forest_file)
-    species_extinction_dataset = load_world_bank_data(species_extinction_file)
+    species_dataset = load_species_data(str(species_file))
+    agriculture_dataset = load_world_bank_data(str(agriculture_file))
+    land_dataset = load_world_bank_data(str(land_file))
+    forest_dataset = load_world_bank_data(str(forest_file))
+    species_extinction_dataset = load_world_bank_data(str(species_extinction_file))
 
     count = create_batches_for_pair_of_days(
         atmospheric_dataset_day1_path=atmospheric_dataset_day1_path,
